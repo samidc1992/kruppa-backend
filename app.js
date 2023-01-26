51fc4d6826bb76c6aec04fcf9f457d8cb20892f3
Sami <s.david.correa@gmail.com>
2023-01-26 20:38:13 +0100

    deployment fix

--- app.js
+++ app.js
@@ -1,39 +1,38 @@
 require('dotenv').config();
-
-const { connection } = require('./models/connection');
+require('./models/connection');
+const mongoose = require('mongoose');
 
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var groupsRouter = require('./routes/groups');
 var sportsRouter = require('./routes/sports');
 const cors = require('cors');
 const fileUpload = require('express-fileupload');
+const connectionString = process.env.CONNECTION_STRING;
 
-const createApp = async () => {  
-    var app = express();
-    await connection();
-    
-    app.use(cors());
+var app = express();
 
-    app.use(fileUpload());
+app.use(cors());
 
-    app.use(logger('dev'));
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: false }));
-    app.use(cookieParser());
-    app.use(express.static(path.join(__dirname, 'public')));
+app.use(fileUpload());
 
-    app.use('/', indexRouter);
-    app.use('/users', usersRouter);
-    app.use('/groups', groupsRouter);
-    app.use('/sports', sportsRouter);
-    
-    return app;
-}
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-module.exports = { createApp };
+app.use('/users', usersRouter);
+app.use('/groups', groupsRouter);
+app.use('/sports', sportsRouter);
+
+mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
+        .then(() => console.log('Database connected'))
+        .catch(error => console.error(error));
+
+module.exports = app;
+
